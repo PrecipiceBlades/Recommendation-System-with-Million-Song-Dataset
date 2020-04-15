f91a2456f7598af88ff87f20e003d824a8ff1655
stevenliu000 <wxabc147@hotmail.com>
2020-04-14 23:27:54 -0400

    add attri

--- draft.py
+++ draft.py
@@ -44,21 +44,40 @@ def read_h5_to_list(filename):
         # METADATA
         song_info.append(float(get_artist_familiarity(h5tocopy,songidx)))
         song_info.append(float(get_artist_hotttnesss(h5tocopy,songidx)))
-        # song_info.append(get_artist_id(h5tocopy,songidx))
-        # song_info.append(get_artist_mbid(h5tocopy,songidx))
-        # song_info.append(get_artist_playmeid(h5tocopy,songidx))
-        # song_info.append(get_artist_7digitalid(h5tocopy,songidx))
-        # song_info.append(get_artist_latitude(h5tocopy,songidx))
-        # song_info.append(get_artist_location(h5tocopy,songidx))
-        # song_info.append(get_artist_longitude(h5tocopy,songidx))
-        # song_info.append(get_artist_name(h5tocopy,songidx))
-        # song_info.append(get_release(h5tocopy,songidx))
-        # song_info.append(get_release_7digitalid(h5tocopy,songidx))
-        # song_info.append(get_song_id(h5tocopy,songidx))
-        # song_info.append(get_song_hotttnesss(h5tocopy,songidx))
-        # song_info.append(get_title(h5tocopy,songidx))
-        song_info.append(float(get_track_7digitalid(h5tocopy,songidx)))
-        
+        song_info.append(str(get_artist_id(h5tocopy,songidx)))
+        song_info.append(str(get_artist_location(h5tocopy,songidx)))
+        song_info.append(get_artist_mbtags(h5tocopy,songidx).tolist())
+        song_info.append(get_artist_mbtags_count(h5tocopy,songidx).tolist())
+        song_info.append(str(get_artist_name(h5tocopy,songidx)))
+        song_info.append(get_artist_terms(h5tocopy,songidx).tolist())
+        song_info.append(get_artist_terms_freq(h5tocopy,songidx).tolist())
+        song_info.append(get_artist_terms_weight(h5tocopy,songidx).tolist())
+        song_info.append(float(get_danceability(h5tocopy,songidx)))
+        song_info.append(float(get_duration(h5tocopy,songidx)))
+        song_info.append(float(get_end_of_fade_in(h5tocopy,songidx)))
+        song_info.append(float(get_energy(h5tocopy,songidx)))
+        song_info.append(float(get_key(h5tocopy,songidx)))
+        song_info.append(float(get_key_confidence(h5tocopy,songidx)))
+        song_info.append(float(get_loudness(h5tocopy,songidx)))
+        song_info.append(float(get_mode(h5tocopy,songidx)))
+        song_info.append(float(get_mode_confidence(h5tocopy,songidx)))
+        song_info.append(str(get_release(h5tocopy,songidx)))
+        song_info.append(get_segments_confidence(h5tocopy,songidx).tolist())        
+        song_info.append(get_segments_loudness_max(h5tocopy,songidx).tolist())        
+        song_info.append(get_segments_loudness_max_time(h5tocopy,songidx).tolist())    
+        song_info.append(get_segments_pitches(h5tocopy,songidx).tolist())    
+        song_info.append(get_segments_timbre(h5tocopy,songidx).tolist())    
+        song_info.append(get_similar_artists(h5tocopy,songidx).tolist())   
+        song_info.append(float(get_artist_hotttnesss(h5tocopy,songidx)))
+        song_info.append(str(get_song_id(h5tocopy,songidx)))
+        song_info.append(float(get_start_of_fade_out(h5tocopy,songidx)))
+        song_info.append(float(get_tempo(h5tocopy,songidx)))
+        song_info.append(int(get_time_signature(h5tocopy,songidx)))
+        song_info.append(float(get_time_signature_confidence(h5tocopy,songidx)))
+        song_info.append(str(get_title(h5tocopy,songidx)))
+        song_info.append(str(get_track_id(h5tocopy,songidx)))
+        song_info.append(int(get_year(h5tocopy,songidx)))
+
         result.append(song_info)
     h5tocopy.close()
     return result
@@ -73,8 +92,17 @@ filenames = getListOfFiles(data_path)
 rdd = sc.parallelize(filenames, num_nodes)
 rdd1 = rdd.flatMap(lambda x: read_h5_to_list(x))
 # TODO: modified with attribute name
-df1 = rdd1.toDF(["a", "b", "c"])
-vectorizer.setInputCols(["a", "b", "c"])
+col_name = ["artist familiarity", "artist hotttnesss", "artist id", "artist location", "artist mbtags", 
+ "artist mbtags count", "artist name", "artist terms", "artist terms freq", "artist terms weight", 
+ "danceability", "duration", "end of fade in", "energy", "key",
+"key confidence", "loudness", "mode", "mode confidence", "release", 
+ "segments confidence", "segments loudness max", "segments loudness max time", 
+"segments pitches", "segments timbre", "similar artists", 
+"song hotttnesss", "song id", "start of fade out", "tempo", "time signature", 
+"time signature confidence", "title", "track id", "year"]
+
+df1 = rdd1.toDF(col_name)
+vectorizer.setInputCols(col_name)
 vectorizer.setOutputCol("features")
 
 
