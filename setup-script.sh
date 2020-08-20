2bc0a7f94ebce3affa620a1ff776485cf80a7462
Rhett Yi <30744601+YiRuitao@users.noreply.github.com>
2020-08-20 17:57:08 -0400

    Update setup-script.sh

--- setup-script.sh
+++ setup-script.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 set -x -e
 
-sudo yum -y install git
-sudo yum -y install htop
+sudo yum -y install git htop
 cd /home/hadoop
 git clone https://github.com/YiRuitao/10605_Group8.git
 cd 10605_Group8
